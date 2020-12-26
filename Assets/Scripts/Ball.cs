ef30f301a4fe30fa2cd283605b588d0b3769d00c
Hebrin <ashram28@gmail.com>
2020-12-26 12:20:10 +0300

    Первичное размещение

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rbBall;
+    float startSpeed = 300;
+    public GameObject startPosition;
+    void Start()
+    {
+        rbBall = GetComponent<Rigidbody2D>();
+
+        StartImpulse();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Ground"))
+        {
+            transform.position = startPosition.transform.position;
+            rbBall.velocity = Vector2.zero;
+                
+        }
+    }
+
+    void StartImpulse()
+    {
+        rbBall.AddForce(Vector2.up * startSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float horizontalMove;
+    float speed = 0.15f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(horizontalMove < 0)
+        {
+            transform.Translate(Vector2.left.normalized * speed);
+        }
+        else if (horizontalMove > 0)
+        {
+            transform.Translate(Vector2.right.normalized * speed);
+        }
+    }
+}
